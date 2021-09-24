d36adcf8df724b5fbe7d23e519e1639f9425904d
boutamente-abdessamad <boutamente.abdessamad@gmail.com>
2021-09-24 12:43:21 +0100

    finish integration

---
+++ dist/js/script.js
@@ -0,0 +1,82 @@
+
+/****************************************/
+/************ Main Scripts **************/
+/****************************************/
+
+$(document).ready(() => {
+
+
+    // start script 
+    try{
+
+        /* do your magic here*/
+
+
+        // best-sellers script 
+
+        // home script 
+        var bsSlider = $('.bs-slider');
+    
+        if (bsSlider.length) {
+            bsSlider.slick({
+                infinite: true,
+                autoplay: true,
+                dots: true,
+                speed: 300,
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                arrows: false,
+                appendDots: $('.bs-slider-dots'),
+                fade: false
+            });
+            $('.bs-prev').click(function (e) {
+                e.preventDefault();
+                bsSlider.slick('slickPrev');
+            });
+            $('.bs-next').click(function (e) {
+                e.preventDefault();
+                bsSlider.slick('slickNext');
+            });
+        }
+        let containerWidth = $('.best-sellers>.container').outerWidth();
+        let viewportWidth = $('body').outerWidth();
+        let containerGutters = ((viewportWidth -  containerWidth)/2)-46;
+        $('.bs-slider').show();
+        $('.bs-slider-dots').css({
+            width : bsSlider.outerWidth() + containerGutters + "px"
+        })
+        bsSlider.css({
+            width : bsSlider.outerWidth() + containerGutters + "px"
+        })
+
+        // menus scripts 
+        $('.has-children').click(function(e){
+            e.preventDefault();
+            $(this).toggleClass('in');
+        } )
+
+        $('.shop-submenu-toggle').click(function(e){
+            e.preventDefault();
+            $('body').toggleClass('shop-submenu-in');
+            $('body').removeClass('services-submenu-in');
+        } )
+
+        $('.services-submenu-toggle').click(function(e){
+            e.preventDefault();
+            $('body').removeClass('shop-submenu-in');
+            $('body').toggleClass('services-submenu-in');
+        } )
+       
+       
+
+    }catch(e){
+        console.log('Error In script detected');
+        console.log('==========================');
+        console.error(e);
+    }
+    // end script 
+
+
+
+
+})
